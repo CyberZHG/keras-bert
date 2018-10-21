623c2a0c2860741745b43859f6dfd36cfcf13d26
CyberZHG <CyberZHG@gmail.com>
2018-10-21 17:07:01 +0800

    Remove no capture

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-nosetests  --nocapture --with-coverage --cover-html --cover-html-dir=htmlcov --cover-package="keras_bert" tests
+nosetests --with-coverage --cover-html --cover-html-dir=htmlcov --cover-package="keras_bert" tests
