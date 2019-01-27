5647a8917d340d44d90b952be682678ba33d33a7
CyberZHG <CyberZHG@gmail.com>
2019-01-27 16:36:22 +0800

    Update setup.py

--- publish.sh
+++ publish.sh
@@ -1,5 +1,2 @@
 #!/usr/bin/env bash
-rm -f README.rst && m2r README.md
-rm -f dist/*
-python3 setup.py sdist
-twine upload dist/*
+rm -rf dist/* && python3 setup.py sdist && twine upload dist/*
--- setup.py
+++ setup.py
@@ -1,21 +1,27 @@
+import codecs
 from setuptools import setup, find_packages
+
+
+with codecs.open('README.md', 'r', 'utf8') as reader:
+    long_description = reader.read()
+
+
+with codecs.open('requirements.txt', 'r', 'utf8') as reader:
+    install_requires = list(map(lambda x: x.strip(), reader.readlines()))
+
 
 setup(
     name='keras-bert',
-    version='0.25.0',
+    version='0.27.0',
     packages=find_packages(),
     url='https://github.com/CyberZHG/keras-bert',
     license='MIT',
     author='CyberZHG',
     author_email='CyberZHG@gmail.com',
     description='BERT implemented in Keras',
-    long_description=open('README.rst', 'r').read(),
-    install_requires=[
-        'numpy',
-        'keras',
-        'keras-pos-embd==0.6.0',
-        'keras-transformer==0.11.0',
-    ],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    install_requires=install_requires,
     classifiers=(
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.6",
