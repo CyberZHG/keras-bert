0e580df5aba302ce3f5f063a703fd38f0bc7491d
苏剑林(Jianlin Su) <bojone@spaces.ac.cn>
2019-05-21 14:53:50 +0800

    add changing activation option  (#63)
    * Update bert.py
    
    * Update bert.py

--- keras_bert/bert.py
+++ keras_bert/bert.py
@@ -77,6 +77,10 @@ def get_model(token_num,
     :param lr: Learning rate.
     :return: The compiled model.
     """
+    if feed_forward_activation == 'gelu':
+        feed_forward_activation = gelu
+    else:
+        keras.activations.get(feed_forward_activation)
     if trainable is None:
         trainable = training
     inputs = get_inputs(seq_len=seq_len)
